a32b4e573a545dd73ce6013baf000d377d43a695
Mikk Noorkõiv <mikk.noorkoiv@me.com>
2018-04-05 20:20:37 +0300

    Initial Commit

---
+++ AppEventCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppEventCount
+//
+//  Created by Mikk Noorkõiv on 05/04/2018.
+//  Copyright © 2018 Mikk Noorkõiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
